6f91a17b2c9e3cc1da69c43281b6ad0a7010d0bb
Talita Lopes <talitabc.lopes@hotmail.com>
2026-05-24 22:40:45 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotelAgenda14/App.xaml.cs
@@ -0,0 +1,53 @@
+﻿using MauiAppHotel.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MauiAppHotel
+{
+    public partial class App : Application
+    {
+        public List<Quarto> lista_quartos = new List<Quarto>
+        {
+            new Quarto()
+            {
+                Descricao = "Suíte Super Luxo",
+                ValorDiarioAdulto = 110.0,
+                ValorDiarioCrianca = 55.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Luxo",
+                ValorDiarioAdulto = 80.0,
+                ValorDiarioCrianca = 40.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Single",
+                ValorDiarioAdulto = 50.0,
+                ValorDiarioCrianca = 25.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Crise",
+                ValorDiarioAdulto = 25.0,
+                ValorDiarioCrianca = 12.5
+            }
+        };
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new Views.ContratacaoHospedagem());
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400;
+            window.Height = 600;
+
+            return window;
+        }
+    }
+}
---
+++ MauiAppHotelAgenda14/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppHotelAgenda14
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
